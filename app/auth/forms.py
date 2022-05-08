a51d1e9a9932e245b36d74db80ac4f4132f9918f
charity-bit <charitynyanchera@gmail.com>
2022-05-08 17:06:17 +0300

    st:post pitch form

---
+++ app/auth/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo
+
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your Username',validators=[DataRequired()])
+    password = PasswordField('Enter Password',validators=[DataRequired(),EqualTo('password_confirm',message='Passwords do not match')])
+    password_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("Email is already in use")
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username already exists')
+
+
+class LoginForm(FlaskForm):
+    email =  StringField('Your EmailAddress',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,24 @@
-from flask import render_template
+import re
+from unicodedata import category
+from flask import render_template, request
+from flask_login import login_required,current_user
 from . import main
+from ..models import Post
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/post-pitch')
+@main.route('/post-pitch',methods = ['GET','POST'])
+# @login_required
 def post():
+    if request.method == 'POST':
+        title = request.form('title')
+        pitch = request.form.get('pitch') 
+        category =  request.form.get('category')
+        
+
+
+
+
     return render_template('post.html')
