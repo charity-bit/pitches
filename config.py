91dd5bbf295f375581c2e018f8005b32825d59d9
charity-bit <charitynyanchera@gmail.com>
2022-05-11 13:12:41 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -8,11 +8,7 @@ class Config:
     SECRET_KEY = 'my_key'
     UPLOADED_PHOTOS_DEST='/app/static/photos'
 
-    EMAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+   
     
 
 class TestConfig(Config):
