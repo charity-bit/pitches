b44dd32b7ee7f56e96dc7415a270f0bb068c38c3
charity-bit <charitynyanchera@gmail.com>
2022-05-11 11:48:57 +0300

    dp

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     UPLOADED_PHOTOS_DEST = os.environ.get('UPLOADED_PHOTOS_DEST')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://","ql://",1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'my_key'
     UPLOADED_PHOTOS_DEST='/app/static/photos'
