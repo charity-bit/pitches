e0b22e100ce025f345b09a9fe29c3f728f389833
charity-bit <charitynyanchera@gmail.com>
2022-05-11 12:48:50 +0300

    dp

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     UPLOADED_PHOTOS_DEST = os.environ.get('UPLOADED_PHOTOS_DEST')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'my_key'
     UPLOADED_PHOTOS_DEST='/app/static/photos'
@@ -19,8 +19,10 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwertyip@localhost/pitch_test'
 
 
+
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    
 
 class DevConfig(Config):
     DEBUG = True
